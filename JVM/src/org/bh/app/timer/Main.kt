abfdbf1ca6debea6dc2ac453fa1d1517c6ff126b
BenLeggiero <BenLeggiero@Gmail.com>
2017-10-01 23:26:36 -0400

    Created IDEA project

---
+++ JVM/src/org/bh/app/timer/Main.kt
@@ -0,0 +1,10 @@
+package org.bh.app.timer
+
+/**
+ * @author Ben Leggiero
+ * @since 2017-10-01
+ */
+
+fun main(args: Array<String>) {
+
+}
---
+++ JVM/src/org/bh/app/timer/ui/BHTimerWindow.kt
@@ -0,0 +1,10 @@
+package org.bh.app.timer.ui
+
+import javafx.stage.Window
+
+/**
+ * @author Ben Leggiero
+ * @since 2017-10-01
+ */
+class BHTimerWindow: LatteWindow {
+}
